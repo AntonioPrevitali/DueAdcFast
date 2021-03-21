a11f85148d512fe709ef1c056e5d941a62fa33f4
AntonioPrevitali <81116769+AntonioPrevitali@users.noreply.github.com>
2021-03-21 23:39:53 +0100

    Update DueAdcFast.h

--- src/DueAdcFast.h
+++ src/DueAdcFast.h
@@ -9,9 +9,7 @@
   Created by Antonio Previtali March, 2019.
   
   This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  modify it under the terms of the Gnu general public license version 3
 
 */
 
